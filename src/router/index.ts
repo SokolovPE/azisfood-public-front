9ee9e64c3e9f2858073defab24a8e83be06b1700
sokolovpe <sokolovp@outlook.com>
2021-12-04 01:01:04 +0300

    Cleanup

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,18 @@
+/* eslint-disable implicit-arrow-linebreak */
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
   {
     path: '/menu',
     name: 'Menu',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue'),
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../modules/catalog/views/CatalogView.vue'),
   },
   {
     path: '/about',
@@ -25,6 +21,11 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '*',
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../modules/catalog/views/CatalogView.vue'),
   },
 ];
 
